b068ecc15e1d170272bf3ea58be44191e0c98b68
Khaled Amirat <khaled.amirat@yahoo.com>
2020-12-06 17:26:47 +0100

    add travis

---
+++ src/test/java/ml/khaledamirat/ci/ContinuousIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.khaledamirat.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuousIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
